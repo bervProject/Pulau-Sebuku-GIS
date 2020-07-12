db6534baa2d4f558454525554bf0fa002407640b
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-07-12 08:41:10 +0700

    Add github button

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -4,8 +4,20 @@
       <b-navbar-item tag="router-link" :to="{ path: '/' }">Pulau Sebuku GIS</b-navbar-item>
     </template>
     <template slot="start">
-        <b-navbar-item tag="router-link" to="/">Home</b-navbar-item>
-        <b-navbar-item tag="router-link" to="/data">Data</b-navbar-item>
+      <b-navbar-item tag="router-link" to="/">Home</b-navbar-item>
+      <b-navbar-item tag="router-link" to="/data">Data</b-navbar-item>
+    </template>
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <b-button
+            tag="a"
+            icon-left="github"
+            icon-pack="fab"
+            href="https://github.com/bervProject/Pulau-Sebuku-GIS"
+          >Github</b-button>
+        </div>
+      </b-navbar-item>
     </template>
   </b-navbar>
 </template>
